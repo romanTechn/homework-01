d79fe5f906e4ae542afc14415ff35578ba3f78d9
romanTechn <rom.okspb@gmail.com>
2021-08-16 14:42:08 +0300

    Updated text

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import reactDom from "react-dom";
 
-const element = <h1>Hello World, меня зовут: Роман Осипов</h1>;
+const element = <h1>Hello World, меня зовут: Роман Осипов!</h1>;
 
 reactDom.render(element, document.getElementById("root"));
